fc0bf8155caff13315599a1ab8973402b065dbf9
anveshanam <161697767+anveshanam@users.noreply.github.com>
2024-03-01 18:01:32 +0530

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,9 @@
+document.getElementById('whatsapp').addEventListener('click',function(){
+    document.getElementById('WAlink').style.display ='flex';
+})
+
+document.getElementsByClassName('close').addEventListener('click', function () {
+   document.getElementById('WAlink').style.display = 'none';
+})
+
+/////////////////
